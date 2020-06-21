0287fdd470aa93034417a55052e8a2f6147056ef
Aljon Santiago <santiagoaj2001@gmail.com>
2020-06-22 03:32:29 +0800

    Journalist to lowercase

--- server/main.lua
+++ server/main.lua
@@ -5,7 +5,7 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 RegisterCommand("cam", function(source, args, raw)          -- Camera Man and Boss Access only
     local src = source                                      -- Added 'xPlayer.job.name == 'Journalist' to prevent other jobs from accessing said props.'
     local xPlayer = ESX.GetPlayerFromId(src)
-    if xPlayer ~= nil and xPlayer.job.name == 'Journalist' then
+    if xPlayer ~= nil and xPlayer.job.name == 'journalist' then
         if xPlayer.job.grade == 1 or xPlayer.job.grade == 4 then
         TriggerClientEvent("Mic:ToggleBMic", src)
         end
@@ -15,7 +15,7 @@ end)
 RegisterCommand("bmic", function(source, args, raw)         -- Crew Access and Boss Access only
     local src = source
     local xPlayer = ESX.GetPlayerFromId(src)
-    if xPlayer ~= nil and xPlayer.job.name == 'Journalist' then
+    if xPlayer ~= nil and xPlayer.job.name == 'journalist' then
         if xPlayer.job.grade == 0 or xPlayer.job.grade == 4 then
         TriggerClientEvent("Mic:ToggleBMic", src)
         end
@@ -25,7 +25,7 @@ end)
 RegisterCommand("mic", function(source, args, raw)          -- Anchor, Reporter Access and Boss Access only
     local src = source
     local xPlayer = ESX.GetPlayerFromId(src)
-    if xPlayer ~= nil and xPlayer.job.name == 'Journalist' then
+    if xPlayer ~= nil and xPlayer.job.name == 'journalist' then
         if xPlayer.job.grade == 2 or xPlayer.job.grade == 3 or xPlayer.job.grade == 4 then
         TriggerClientEvent("Mic:ToggleMic", src)
         end
